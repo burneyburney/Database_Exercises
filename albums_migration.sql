9fb5bfb5b0f6df41578881f8e35b1bd1f142c80c
Michael Burney <burney318@gmail.com>
2016-03-14 15:07:19 -0500

    creation and tests of SQL exercises migration/seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,21 @@
+-- Michael Burney
+-- March 14th 2016
+-- My Sql first exercise
+
+-- perform actions here.
 USE codeup_test_db;
 
+
+-- if there is already one created, destroy it.
+DROP TABLE IF EXISTS albums;
+
+-- create a table named 'albums'
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name   VARCHAR(50) NOT NULL,
+    release_date DATE,
+    sales_in_millions FLOAT(10, 2),
+    genre VARCHAR(50),
+	PRIMARY KEY (id)
+);
