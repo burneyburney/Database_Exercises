f1837f0148bd7ccc95d3a047904276d685c67886
Michael Burney <burney318@gmail.com>
2016-03-15 15:56:04 -0500

    practicing group by in SQL

---
+++ group_by_exercise.sql
@@ -0,0 +1,32 @@
+SELECT first_name, last_name
+FROM employees
+WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
+ORDER BY last_name desc, first_name desc;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no desc;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%'
+ORDER BY hire_date desc;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+
