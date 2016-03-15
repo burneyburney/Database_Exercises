311108e99033a490bed0ae10cde5c3c7aa7a77c4
Michael Burney <burney318@gmail.com>
2016-03-15 14:13:55 -0500

    addition of where_exercises.sql FINISHED

---
+++ where_exercises.sql
@@ -0,0 +1,19 @@
+SELECT first_name
+FROM employees
+WHERE  first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%%%%-12-25';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
