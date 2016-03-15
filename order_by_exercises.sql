0cc418db3008ec5e66aba42d81522235bde31644
Michael Burney <burney318@gmail.com>
2016-03-15 15:23:31 -0500

    added order to exercises as well as played with asc and desc

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,11 +1,12 @@
-SELECT first_name
+SELECT first_name, last_name
 FROM employees
-WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
-
+WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
+ORDER BY last_name desc, first_name desc;
 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no desc;
 
 SELECT first_name, last_name
 FROM employees
@@ -17,7 +18,8 @@ WHERE hire_date LIKE '199%';
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%';
+WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%'
+ORDER BY hire_date desc;
 
 SELECT first_name, last_name
 FROM employees
