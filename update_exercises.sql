070c332a6dfe90ffd827cd2b606e82abab7a4a98
Michael Burney <burney318@gmail.com>
2016-03-15 11:07:14 -0500

    update to seeder/exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -7,10 +7,8 @@ USE codeup_test_db
 -- Write SELECT statements to output each of the following with a caption:
     -- All albums in your table.
     SELECT * FROM albums;
-
     -- All albums released before 1980
     SELECT name AS 'Albums released before 1980' FROM albums WHERE release_date < '1980';
-
     -- All albums by Michael Jackson
     SELECT name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
 
@@ -25,17 +23,16 @@ USE codeup_test_db
     -- aparently this doesnt work
     UPDATE albums
     SET release_date = '1800-01-01'
-    WHERE release_date FROM albums WHERE release_date < '1980';
+    WHERE release_date FROM albums WHERE release_date < '1980'
     SELECT release_date FROM albums WHERE release_date < '1980';
 
 -- Change "Michael Jackson" to "Peter Jackson"
+    SELECT * FROM albums WHERE artist = 'Michael Jackson';
+    
     UPDATE albums
     SET artist = 'Peter Jackson'
     WHERE id = 1
-    SELECT * FROM albums WHERE artist = 'Michael Jackson'
     SELECT * FROM albums WHERE artist = 'Peter Jackson';
-
-
 
 --> 1,11,17  <-- MICHAEL JACKSON id locations
 
