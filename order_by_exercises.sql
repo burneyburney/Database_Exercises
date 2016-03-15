d36d515af33dc66bb7932ab0db44582d04158933
Michael Burney <burney318@gmail.com>
2016-03-15 15:15:43 -0500

    initial commit of order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,28 @@
+SELECT first_name
+FROM employees
+WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
