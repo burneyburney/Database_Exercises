13cc85e982d3c1a2b361937582dc0dc51fee2d24
Michael Burney <burney318@gmail.com>
2016-03-15 15:08:35 -0500

    update to where_exercises.sql (finishing up)

--- where_exercises.sql
+++ where_exercises.sql
@@ -25,4 +25,4 @@ WHERE last_name LIKE '%q%';
 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%' AND NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
