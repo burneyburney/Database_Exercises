506649a564cae125758340157f65d0fedd05cddd
Michael Burney <burney318@gmail.com>
2016-03-15 14:57:30 -0500

    update to where_exercises.sql

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,15 @@
 SELECT first_name
 FROM employees
-WHERE  first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
 
 SELECT first_name, last_name
 FROM employees
@@ -12,8 +17,12 @@ WHERE hire_date LIKE '199%';
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date LIKE '%%%%-12-25';
+WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%';
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT LIKE '%qu%';
