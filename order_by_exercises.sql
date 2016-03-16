e68c9f953d6c78ae111b0662241e87448a3ab3d1
Michael Burney <burney318@gmail.com>
2016-03-16 11:15:37 -0500

    completion of lesson (functions)exercises), the name would not update upon save

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,14 +1,15 @@
-SELECT first_name, last_name
+-- functions_exercise.sql
+SELECT count(first_name),gender
 FROM employees
-WHERE  first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M'
-ORDER BY last_name desc, first_name desc;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') 
+GROUP BY gender;
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
 ORDER BY emp_no desc;
 
-SELECT first_name, last_name
+SELECT concat(first_name, last_name)
 FROM employees
 WHERE last_name LIKE 'E%E';
 
@@ -17,15 +18,20 @@ FROM employees
 WHERE hire_date LIKE '199%';
 
 -- employees born on christmas and hired in 90's
-SELECT first_name, last_name
+SELECT first_name, last_name, datediff(now(), hire_date) AS 'days employed'
 FROM employees
-WHERE birth_date LIKE '%%%%-12-25' AND hire_date LIKE '199%'
-ORDER BY hire_date DESC
-LIMIT 5;
+WHERE hire_date BETWEEN '1990-01-01' 
+	AND '1999-12-31'
+	AND birth_date LIKE '%-12-25'	
+ORDER BY birth_date ASC, hire_date DESC;
+
+
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+
 
 SELECT first_name, last_name
 FROM employees
